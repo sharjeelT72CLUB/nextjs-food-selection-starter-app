95a85478d2e291a690b33c2eed38223cecb48b09
sharjeelsk <sharjeelarts@gmail.com>
2024-08-27 23:57:53 +0530

    initial commit

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Meals({params}) {
+  return (
+    <div>Meals
+
+      {params.slug}
+    </div>
+  )
+}
---
+++ app/meals/layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MealsLayout({children}) {
+  return (
+    <div>MealsLayout
+        {children}
+    </div>
+  )
+}
